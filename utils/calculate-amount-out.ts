e1d3afe4b95712526f62a5b2d4bc1aef4e975f28
Andris <106549955+AndRockSYS@users.noreply.github.com>
2024-04-30 15:29:40 +0300

    convert amounts based on decimals and amountIn

---
+++ utils/calculate-amount-out.ts
@@ -0,0 +1,19 @@
+import Pool from '../classes/Pool';
+
+export default async function calculateAmounts(
+    { action, amountIn, token }: SwapRequest,
+    pool: Pool
+): Promise<string[]> {
+    const price = await pool.getPrice(action, token);
+    let amountOut = amountIn * price;
+
+    const decimalsIn = action == Action.Buy ? 18 : token.decimals;
+    const decimalsOut = action == Action.Sell ? 18 : token.decimals;
+
+    return [toFullLength(amountIn, decimalsIn), toFullLength(amountOut, decimalsOut)];
+}
+
+function toFullLength(amountIn: number, decimals: number): string {
+    const amount = Math.floor(amountIn * 10 ** decimals);
+    return amount.toLocaleString('fullwide', { useGrouping: false });
+}
