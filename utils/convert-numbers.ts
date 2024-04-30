0ee40e7fc00ac1fc01ed4b9dcfbc7faa69bdbbaa
Andris <106549955+AndRockSYS@users.noreply.github.com>
2024-04-30 13:17:10 +0300

    added utils to convert numbers to readable text

---
+++ utils/convert-numbers.ts
@@ -0,0 +1,32 @@
+function convertPrice(price: number): string {
+    return price <= 1e-19
+        ? (price / 1e-19).toFixed(4) + '*10⁻¹⁹'
+        : price <= 1e-16
+        ? (price / 1e-16).toFixed(4) + '*10⁻¹⁶'
+        : price <= 1e-13
+        ? (price / 1e-13).toFixed(4) + '*10⁻¹³'
+        : price <= 1e-11
+        ? (price / 1e-11).toFixed(4) + '*10⁻¹¹'
+        : price <= 1e-9
+        ? (price / 1e-9).toFixed(4) + '*10⁻⁹'
+        : price <= 1e-6
+        ? (price / 1e-6).toFixed(4) + '*10⁻⁶'
+        : price <= 1e-3
+        ? (price / 1e-3).toFixed(4) + '*10⁻³'
+        : price.toFixed(4);
+}
+
+function convertAmount(amount: number): string {
+    return amount >= 1e9
+        ? (amount / 1e9).toFixed(2) + 'B'
+        : amount >= 1e6
+        ? (amount / 1e6).toFixed(2) + 'M'
+        : amount >= 1e3
+        ? (amount / 1e3).toFixed(2) + 'K'
+        : amount.toFixed(4);
+}
+
+export default {
+    convertPrice,
+    convertAmount,
+};
