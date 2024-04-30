327d62d313757b8d7e07d824c753f2ed219be3d1
Andris <106549955+AndRockSYS@users.noreply.github.com>
2024-04-30 22:45:31 +0300

    added function to listen to token address and give all information about it

--- index.ts
+++ index.ts
@@ -1,8 +1,12 @@
 import { Telegraf } from 'telegraf';
-import { HDNodeWallet, JsonRpcProvider, Wallet } from 'ethers';
+import { ethers, HDNodeWallet, JsonRpcProvider, Wallet } from 'ethers';
 import dotenv from 'dotenv';
 
 import Token from './class/Token';
+import Pool from './class/Pool';
+import { Action } from './enum';
+
+import { convertPrice, convertAmount } from './utils/convert-numbers';
 
 dotenv.config();
 
@@ -15,14 +19,11 @@ const privateKey = /(0x.{64})/m;
 const address = /(0x.{40})/gm;
 
 bot.start((ctx) => {
-    ctx.sendMessage(
-        'Welcome to the bot! Provide a private key or a new wallet will be generated.',
-        {
-            reply_markup: {
-                inline_keyboard: [[{ text: 'Create new wallet', callback_data: 'generator' }]],
-            },
-        }
-    );
+    ctx.sendMessage('Welcome to the trading bot! \nProvide a private key or generate a new one.', {
+        reply_markup: {
+            inline_keyboard: [[{ text: 'Create new wallet', callback_data: 'generator' }]],
+        },
+    });
 });
 
 bot.action('generator', async (ctx) => {
@@ -44,8 +45,28 @@ bot.hears(privateKey, async (ctx) => {
 
 bot.hears(address, async (ctx) => {
     const token = new Token(provider, ctx.message.text);
+    await token.initialize();
 
-    ctx.sendMessage(`${await token.symbol()} ${await token.decimals()}`);
+    const pool = new Pool(provider, ctx.message.text);
+    await pool.initialize();
+
+    if (pool.address == ethers.ZeroAddress) {
+        ctx.sendMessage('The bot cannot support this token');
+        return;
+    }
+
+    const price = await pool.getPrice(Action.Buy, token);
+    const balance = wallet ? await token.getBalance(wallet.address) : 0;
+    const marketCap = await token.getMarketCap();
+    const volume = await token.getVolumeLastHour();
+
+    ctx.sendMessage(
+        `Token - ${token.symbol} 
+		\n${convertPrice(price)} WETH/${token.symbol} 
+		\nMCap - ${convertAmount(marketCap)}
+		\nLast Hour - ${volume}
+		\nBalance - ${convertAmount(balance)} ${token.symbol}`
+    );
 });
 
 bot.launch();
