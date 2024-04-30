74801dac4eafd587e4be8adf200848b457dece3d
Andris <106549955+AndRockSYS@users.noreply.github.com>
2024-04-30 15:29:07 +0300

    added types for Uniswap Version, Action, Swap request and params for V3 swap

---
+++ types.d.ts
@@ -0,0 +1,26 @@
+enum Version {
+    V2,
+    V3,
+}
+
+enum Action {
+    Buy,
+    Sell,
+}
+
+type SwapRequest = {
+    action: Action;
+    amountIn: number;
+    token: Token;
+};
+
+type ExactInputSingleParams = {
+    tokenIn: string;
+    tokenOut: string;
+    fee: number;
+    recipient: string;
+    deadline: number;
+    amountIn: string;
+    amountOutMinimum: string;
+    sqrtPriceLimitX96: number;
+};
