83a0a4f9f1d138d15ea15986c547ffc65245172f
Andris <106549955+AndRockSYS@users.noreply.github.com>
2024-04-30 13:17:36 +0300

    added Token class to get infor about the token

---
+++ classes/Token.ts
@@ -0,0 +1,34 @@
+import { ethers, JsonRpcProvider, ZeroAddress } from 'ethers';
+
+import ERC20 from '../ABI/Token/ERC-20.json';
+
+export default class Token {
+    contract: ethers.Contract;
+
+    symbol: string = 'UNDEFINED';
+    decimals: number = 18;
+
+    constructor(provider: JsonRpcProvider, address: string) {
+        this.contract = new ethers.Contract(address, ERC20, provider);
+
+        this.contract.symbol().then((symbol) => {
+            this.symbol = symbol;
+        });
+
+        this.contract.decimals().then((decimals) => {
+            this.decimals = decimals;
+        });
+    }
+
+    async getMarketCap(): Promise<bigint> {
+        const totalSupply: bigint = await this.contract.totalSupply();
+
+        const contractAddress = await this.contract.getAddress();
+        const contractBalance: bigint = await this.contract.balanceOf(contractAddress);
+
+        const burnt: bigint = await this.contract.balanceOf(ZeroAddress);
+
+        const marketCap = totalSupply - burnt - contractBalance;
+        return ethers.parseUnits(marketCap.toString(), this.decimals);
+    }
+}
