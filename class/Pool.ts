4cb64148f6441202effde8ec5122d0bf7fb68c40
Andris <106549955+AndRockSYS@users.noreply.github.com>
2024-04-30 22:44:52 +0300

    added function to initialize variables from async methods

--- class/Pool.ts
+++ class/Pool.ts
@@ -10,11 +10,13 @@ import PoolABI from '../ABI/UniswapV3/Pool.json';
 import FactoryV2 from '../ABI/UniswapV2/Factory.json';
 import FactoryV3 from '../ABI/UniswapV3/Factory.json';
 
+import { Version, Action } from '../enum';
+
 export default class Pool {
     provider: JsonRpcProvider;
 
     address: string = ZeroAddress;
-    contract: Contract;
+    contract: Contract = new Contract(ZeroAddress, PairABI);
     version: Version = Version.V2;
 
     tokenAddress: AddressLike;
@@ -22,48 +24,40 @@ export default class Pool {
     constructor(provider: JsonRpcProvider, tokenAddress: AddressLike) {
         this.provider = provider;
         this.tokenAddress = tokenAddress;
+    }
 
-        this.checkStandard(tokenAddress).then(([poolAddress, version]) => {
-            this.address = poolAddress;
-            this.version = version;
-        });
+    async initialize() {
+        let factory = new Contract(FACTORY_V2, FactoryV2, this.provider);
+        let poolAddress = await factory.getPair(this.tokenAddress, WETH);
+
+        if (poolAddress == ZeroAddress) {
+            factory = new Contract(FACTORY_V3, FactoryV3, this.provider);
+            poolAddress = await factory.getPool(this.tokenAddress, WETH, 3000);
+            this.version = Version.V3;
+        }
+
+        this.address = poolAddress;
 
         this.contract = new Contract(
             this.address,
             this.version == Version.V2 ? PairABI : PoolABI,
-            provider
+            this.provider
         );
     }
 
-    private async checkStandard(tokenAddress: AddressLike): Promise<[string, Version]> {
-        let factory = new Contract(FACTORY_V2, FactoryV2, this.provider);
-        let poolAddress = await factory.getPair(tokenAddress, WETH);
-
-        if (poolAddress != ZeroAddress) return [poolAddress, Version.V2];
-
-        factory = new Contract(FACTORY_V3, FactoryV3, this.provider);
-        poolAddress = await factory.getPool(tokenAddress, WETH, 3000);
-
-        return [poolAddress, Version.V3];
-    }
-
     async getPrice(action: Action, token: Token): Promise<number> {
-        const tokenDecimals = await token.decimals();
-
         if (this.version == Version.V2) {
             const [reserve0, reserve1, _timestamp]: bigint[] = await this.contract.getReserves();
 
-            const WETHReserve = reserve1 * ethers.parseEther('1');
-            const tokenReserve = reserve0 * BigInt(10) ** BigInt(tokenDecimals);
+            const tokenRes = parseFloat(ethers.formatUnits(reserve0, token.decimals));
+            const WETHRes = parseFloat(ethers.formatUnits(reserve1, 18));
 
-            return action == Action.Buy
-                ? Number(WETHReserve / tokenReserve)
-                : Number(tokenReserve / WETHReserve);
+            return action == Action.Buy ? WETHRes / tokenRes : tokenRes / WETHRes;
         } else {
             const info = await this.contract.slot0();
             const token0 = await this.contract.token0();
 
-            let price = (info.sqrtPriceX96 / 2 ** 96) ** 2 / 10 ** (18 - tokenDecimals);
+            let price = (info.sqrtPriceX96 / 2 ** 96) ** 2 / 10 ** (18 - token.decimals);
             //handle if the token pair was set in a wrong way
             if (token0 == token.address) price = 1 / price;
 
