d199b83a391880258d99d2f84b5196cafa15fbc2
Andris <106549955+AndRockSYS@users.noreply.github.com>
2024-05-01 19:36:14 +0300

    added type Slot0 + fixed getPrice for Uniswap V3

--- class/Pool.ts
+++ class/Pool.ts
@@ -1,6 +1,6 @@
 import { AddressLike, Contract, ethers, JsonRpcProvider, ZeroAddress } from 'ethers';
 
-import { Version, Action } from 'types';
+import { Version, Action, Slot0 } from 'types';
 
 import Token from 'class/Token';
 
@@ -53,11 +53,12 @@ export default class Pool {
 
             return action == Action.Buy ? WETHRes / tokenRes : tokenRes / WETHRes;
         } else {
-            const info = await this.contract.slot0();
-            console.log(info);
+            const slot0: Slot0 = await this.contract.slot0();
             const token0 = await this.contract.token0();
 
-            let price = (info.sqrtPriceX96 / 2 ** 96) ** 2 / 10 ** (18 - token.decimals);
+            let price =
+                (parseInt(slot0.sqrtPriceX96.toString()) / Math.pow(2, 96)) ** 2 /
+                Math.pow(10, 18 - token.decimals);
             //handle if the token pair was set in a wrong way
             if (token0 == token.address) price = 1 / price;
 
