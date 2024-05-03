49717d488e9390e70d0cd13919504c93c4fcae66
Andris <106549955+AndRockSYS@users.noreply.github.com>
2024-05-03 12:17:48 +0300

    added buttons to list all the telegram buttons that are in use

---
+++ telegram/buttons.ts
@@ -0,0 +1,9 @@
+const generateWallet = { text: 'Create new wallet', callback_data: 'generator' };
+
+const buyTokens = { text: 'Buy', callback_data: 'buy' };
+const sellTokens = { text: 'Sell', callback_data: 'sell' };
+
+const withdrawWETH = { text: 'Withdraw WETH', callback_data: 'withdraw-weth' };
+const depositETH = { text: 'Deposit ETH', callback_data: 'deposit-eth' };
+
+export { generateWallet, buyTokens, sellTokens, withdrawWETH, depositETH };
