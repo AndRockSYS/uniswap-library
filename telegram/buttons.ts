b6fabc9906d6c1cb124d4a5ba82cdaf2a092435d
Andris <106549955+AndRockSYS@users.noreply.github.com>
2024-05-04 13:48:57 +0300

    replace keyboard with function to return keyboard for specific token with query

--- telegram/buttons.ts
+++ telegram/buttons.ts
@@ -6,4 +6,20 @@ const sellTokens = { text: 'Sell', callback_data: 'sell' };
 const withdrawWETH = { text: 'Withdraw WETH', callback_data: 'withdraw-weth' };
 const depositETH = { text: 'Deposit ETH', callback_data: 'deposit-eth' };
 
-export { generateWallet, buyTokens, sellTokens, withdrawWETH, depositETH };
+function getPercentageKeyboard(addressOrSymbol: string): {
+    text: string;
+    callback_data: string;
+}[][] {
+    return [
+        [
+            { text: '25%', callback_data: `${addressOrSymbol}-0.25` },
+            { text: '50%', callback_data: `${addressOrSymbol}-0.75` },
+        ],
+        [
+            { text: '75%', callback_data: `${addressOrSymbol}-0.5` },
+            { text: '100%', callback_data: `${addressOrSymbol}-1` },
+        ],
+    ];
+}
+
+export { generateWallet, buyTokens, sellTokens, withdrawWETH, depositETH, getPercentageKeyboard };
