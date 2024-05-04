433d8684611e0876d97a2e6411a44f44d423e950
Andris <106549955+AndRockSYS@users.noreply.github.com>
2024-05-04 14:23:17 +0300

    added action arg for buttons to check whenether user wants to buy or sell tokens

--- telegram/buttons.ts
+++ telegram/buttons.ts
@@ -6,18 +6,21 @@ const sellTokens = { text: 'Sell', callback_data: 'sell' };
 const withdrawWETH = { text: 'Withdraw WETH', callback_data: 'withdraw-weth' };
 const depositETH = { text: 'Deposit ETH', callback_data: 'deposit-eth' };
 
-function getPercentageKeyboard(addressOrSymbol: string): {
+function getPercentageKeyboard(
+    addressOrSymbol: string,
+    action?: string
+): {
     text: string;
     callback_data: string;
 }[][] {
     return [
         [
-            { text: '25%', callback_data: `${addressOrSymbol} 0.25` },
-            { text: '50%', callback_data: `${addressOrSymbol} 0.5` },
+            { text: '25%', callback_data: `${addressOrSymbol} 0.25 ${action}` },
+            { text: '50%', callback_data: `${addressOrSymbol} 0.5 ${action}` },
         ],
         [
-            { text: '75%', callback_data: `${addressOrSymbol} 0.75` },
-            { text: '100%', callback_data: `${addressOrSymbol} 1` },
+            { text: '75%', callback_data: `${addressOrSymbol} 0.75 ${action}` },
+            { text: '100%', callback_data: `${addressOrSymbol} 1 ${action}` },
         ],
     ];
 }
